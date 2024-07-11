8650ebd3cda3e6f6dcef5ef44bfd5dc6249e16f2
Henry Leao <henrylsilveira@gmail.com>
2024-07-11 19:09:01 -0300

    Segundo dia

--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -1,4 +1,5 @@
 import { MapPin, Calendar, Settings2, ArrowRight } from "lucide-react";
+import { Button } from "../../../components/button";
 
 interface DestinationAndDateStepProps {
   isGuestsInputOpen: boolean,
@@ -17,10 +18,14 @@ export function DestinationAndDateStep({ isGuestsInputOpen, openGuestsInput }: D
       </div>
       <div className="w-px h-6 bg-zinc-800" />
       {isGuestsInputOpen ? (
-        <button onClick={openGuestsInput} className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">Alterar local/data<Settings2 className="size-5" /></button>
-      ) : (
+        <Button onClick={openGuestsInput} variant="secondary">
+          Alterar local/data<Settings2 className="size-5" />
+        </Button>
 
-        <button onClick={openGuestsInput} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">Continuar <ArrowRight className="size-5" /></button>
+      ) : (
+        <Button onClick={openGuestsInput} variant="primary">
+          Continuar <ArrowRight className="size-5" />
+        </Button>
       )}
 
     </div>
--- src/pages/create-trip/steps/invite-guests-step.tsx
+++ src/pages/create-trip/steps/invite-guests-step.tsx
@@ -1,11 +1,12 @@
 import { UserRoundPlus, ArrowRight } from "lucide-react";
+import { Button } from "../../../components/button";
 
 interface InviteGuestsStepProps {
     openGuestsModal: () => void,
     emailsToInvite: string[],
     openConfirmTripModal: () => void
 }
-export function InviteGuestsStep({openGuestsModal, emailsToInvite, openConfirmTripModal}: InviteGuestsStepProps) {
+export function InviteGuestsStep({ openGuestsModal, emailsToInvite, openConfirmTripModal }: InviteGuestsStepProps) {
     return (
         <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
             <button type="button" onClick={openGuestsModal} className="flex items-center gap-2 flex-1 text-left">
@@ -18,7 +19,10 @@ export function InviteGuestsStep({openGuestsModal, emailsToInvite, openConfirmTr
             </button>
 
             <div className="w-px h-6 bg-zinc-800" />
-            <button onClick={openConfirmTripModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">Confirmar viagem <ArrowRight className="size-5" /></button>
+            <Button onClick={openConfirmTripModal} variant="primary">
+                Confirmar viagem <ArrowRight className="size-5" />
+            </Button>
+            
         </div>
     )
 }
